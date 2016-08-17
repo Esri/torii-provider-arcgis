cdc4f8de716985aaef42dda4604283eac099b61d
mjuniper <mjuniper@gmail.com>
2016-08-17 16:14:46 -0600

    add release config

--- config/release.js
+++ config/release.js
@@ -3,19 +3,7 @@
 
 // For details on each option run `ember help release`
 module.exports = {
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // publish: true,
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
+  remote: 'upstream',
+  annotation: 'Release %@',
+  message: 'Bumped version to %@',
 };

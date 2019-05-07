8ea7c34ddca6197a5114d9fb1a5b5d9a2035f3fe
john gravois <jgravois@esri.com>
2019-05-07 11:10:56 -0700

    bump to rest-js v2.0.0 and use ember-auto-import (#66)

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 /*
- * Copyright (c) 2016-2018 Esri
+ * Copyright (c) 2016-2019 Esri
  * Apache-2.0
 */
 
 /* eslint-env node */
 'use strict';
-
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
 
@@ -16,34 +12,5 @@ module.exports = {
 
   isDevelopingAddon: function () {
     return false;
-  },
-
-  included( /* app */ ) {
-    this._super.included.apply(this, arguments);
-    // bundle scripts from vendor folder
-    this.import('vendor/@esri/arcgis-rest-request/request.umd.js');
-    this.import('vendor/@esri/arcgis-rest-auth/auth.umd.js');
-    this.import('vendor/shims/@esri/arcgis-rest-request.js');
-    this.import('vendor/shims/@esri/arcgis-rest-auth.js');
-  },
-
-  treeForVendor(vendorTree) {
-    var arcgisRequestTree = new Funnel(path.dirname(require.resolve('@esri/arcgis-rest-request/dist/umd/request.umd.js')), {
-      files: ['request.umd.js', 'request.umd.js.map'],
-      destDir: '@esri/arcgis-rest-request'
-    });
-
-    var arcgisAuthTree = new Funnel(path.dirname(require.resolve('@esri/arcgis-rest-auth/dist/umd/auth.umd.js')), {
-      files: ['auth.umd.js', 'auth.umd.js.map'],
-      destDir: '@esri/arcgis-rest-auth'
-    });
-
-    var treesToMerge = [arcgisRequestTree, arcgisAuthTree];
-    // if we got a vendorTree, and add it in
-    if (vendorTree) {
-      treesToMerge.unshift(vendorTree);
-    }
-
-    return new MergeTrees(treesToMerge);
   }
 };

3e3a739aada4416de6cdfaf92900b1cecf6f849e
Dave Bouwman <dave@davebouwman.com>
2023-10-04 12:33:00 -0600

    Bumped version to v4.1.3

--- config/release.js
+++ config/release.js
@@ -8,8 +8,8 @@
 
 // For details on each option run `ember help release`
 module.exports = {
-  remote: 'origin',
-  annotation: 'Release %@',
-  message: 'Bumped version to %@',
-  publish: true
+  remote: "origin",
+  annotation: "Release %@",
+  message: "Bumped version to %@",
+  // publish: true
 };
